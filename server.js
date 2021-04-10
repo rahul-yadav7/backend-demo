87428ac557d01872d399b0c695d273e8faa904fa
nikhil-khurana <nikhil.khurana@ongraph.com>
2021-04-10 12:33:48 +0530

    add route

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express()
 
+app.get('/status', (req, res)=> {
+    res.json({status: 'working ...'})
+    
+})
+
 app.listen(3000, ()=>{
     console.log('rahul working;')
 });
